c17720abac692bee3151917b03fc7d264bb1bfa6
Henrique Dias Leite <hdias8969@gmail.com>
2026-04-14 14:36:41 -0300

    Add feedback message using GET parameter

--- index.php
+++ index.php
@@ -11,3 +11,8 @@
 
 <body class="bg-gray-100 p-8">
     <div class="max-w-4xl mx-auto">
+        <div class="flex justify-between items-center mb-6">
+            <h1 class="text-2xl font-bold text-gray-800">CRUD de Produtos</h1>
+            <a href="create.php" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                + Novo Produto
+            </a>
