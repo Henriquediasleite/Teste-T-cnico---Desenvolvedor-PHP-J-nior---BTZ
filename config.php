b155865d1bfd7e2154c48c1a09f525ea832efe26
Henrique Dias Leite <hdias8969@gmail.com>
2026-04-14 13:44:28 -0300

    Configuração de conexão com banco de dados

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$dbname = "crud_produtos";
+$user = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro de conexão: " . $e->getMessage());
+}
